27f2ab4604f332b995dbd414893e7f7ea2890f1b
leandrochs <leandrochagask@gmail.com>
2023-05-28 13:40:36 -0300

    feat: configuracoes iniciais

---
+++ src/test/kotlin/me/dio/credit/application/system/service/CustomerServiceTest.kt
@@ -0,0 +1,11 @@
+package me.dio.credit.application.system.service
+
+import io.mockk.MockKException
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@ExtendWith(MockKException::class)
+class CustomerServiceTest {
+
+}
